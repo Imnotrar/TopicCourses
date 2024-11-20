c068e964e3c0081ff7a097d011aa0198da19f270
imnotrar <rgumaranti@gmail.com>
2024-11-20 20:49:03 +0700

    initial project - topic courses

---
+++ app/src/main/java/trpl/nim234311050/topic/model/Topic.kt
@@ -0,0 +1,11 @@
+package trpl.nim234311050.topic.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
+
